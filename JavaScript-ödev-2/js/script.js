17d44004f001ffc0ecae3f451c1ee06af9123458
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2021-07-25 16:12:41 +0300

    check, add and remove list element working

--- JavaScript-ödev-2/js/script.js
+++ JavaScript-ödev-2/js/script.js
@@ -19,3 +19,37 @@ for (let i = 0; i < close.length; i++) {
 	}
 }
 
+// checked
+
+ul.addEventListener('click', (e) => {
+	if (e.target.tagName === 'LI') {
+		e.target.classList.toggle('checked');
+	}
+}, false);
+
+
+function newElement() {
+	var listElement = document.createElement("li");
+	var inputValue = document.getElementById("task").value;
+	var t = document.createTextNode(inputValue);
+	listElement.appendChild(t);
+	if (inputValue === '') {
+	  alert("You must write something!");
+	} else {
+	  document.getElementById("list").appendChild(listElement);
+	}
+	document.getElementById("task").value = "";
+  
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	listElement.appendChild(span);
+  
+	for (i = 0; i < close.length; i++) {
+	  close[i].onclick = function() {
+		var div = this.parentElement;
+		div.style.display = "none";
+	  }
+	}
+  }

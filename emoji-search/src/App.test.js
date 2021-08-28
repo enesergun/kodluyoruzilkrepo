1a7439af72c85ac2adb3f27e9c95dcfe8082c108
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2021-08-28 14:57:43 +0300

    header ve emojiler basarili render edilyor

--- emoji-search/src/App.test.js
+++ emoji-search/src/App.test.js
@@ -1,25 +1,22 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom'
 
 import Header from './Header';
+import App from './App'
+import EmojiResults from './EmojiResults'
 import EmojiResultRow from './EmojiResultRow'
 
-describe('emoji search app testleri', () => {
-  
-  beforeEach(() => {
-    render(<Header />)
 
-  });
+test("header basarili sekilde render edilmeli", () => {
+  render(<Header />);
+})
 
-  test("header kısmı render edilmeli", () => {
-    const headerContext = screen.getByText('Emoji Search')
-    expect(headerContext).toBeInTheDocument();
-
-  });
-
- 
-
+test("emojiler basarili sekilde render edilmeli", () => {
+  render(<App />);
+  const emojiWrapper = screen.getByTestId("all-emojies");
+  expect(emojiWrapper.childNodes.length).toEqual(20);
 });
 
